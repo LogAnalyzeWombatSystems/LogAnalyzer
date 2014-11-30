d220d4bb4f55419bd074683dbf86a0bd057c1880
JWarmhold <elsix79@googlemail.com>
2014-11-30 14:06:50 +0100

    Should be totally compatible to eclipse now. (well maybe you need to change the run-config after import ;P  )

---
+++ src/LogAnalyzer.c
@@ -0,0 +1,14 @@
+/*
+ * LogAnalyzer.c
+ *
+ *  Created on: Nov 30, 2014
+ *      Author: WombatSystems
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	puts("Hello World!");
+	return 0;
+}
